f633bd0d5bc50e4fb2b5b5bc4961ab2b43d0b42b
q1206577452 <1206577452@qq.com>
2020-10-12 00:13:12 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,17 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+#package
+cd package/lean
+git clone https://github.com/openwrt-develop/luci-theme-atmaterial
+cd ../..
+
+#update & install feeds
+./scripts/feeds update -a && ./scripts/feeds install -a
+
+#update kernel
+#sed -i 's/KERNEL_PATCHVER:=4.9/KERNEL_PATCHVER:=5.4/g' target/linux/ar71xx/Makefile
+
+#set theme
+sed -i '/set luci.main.mediaurlbase=\/luci-static\/bootstrap/d' feeds/luci/themes/luci-theme-bootstrap/root/etc/uci-defaults/30_luci-theme-bootstrap
